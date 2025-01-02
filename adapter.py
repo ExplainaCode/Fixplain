6d9fccf38e36088ce11b16b78700c1be4bb2f6b2
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-02 09:23:28 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -89,10 +89,11 @@ class LLamaAdapter(nn.Module):
             """
             layer_id = 0
             for layer in repairllama.base_model.model.model.layers:
+                print(f"Layer {layer_id}: {layer}")
                 attention_layer = layer.self_attn
+                print(f"Registering hook on layer {i}: {attention_layer}")
                 attention_layer.layer_id = layer_id  # Tag the layer with an ID
                 attention_layer.register_forward_hook(self._hook_fn)
-                print("inside loop layer_id: ", layer_id)
                 layer_id += 1
 
         return repairllama, tokenizer
