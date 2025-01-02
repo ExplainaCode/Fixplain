16b106bb1a6a5e5b8031778bf8854b6dc71200a8
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-02 18:40:08 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -215,8 +215,8 @@ class LLamaAdapter(nn.Module):
         # Processing RepairLLama output
         repairllama_h  = self.repairllama.model.model.norm(repairllama_h[0])
         print("after norm", repairllama_h)
-        repairllama_h, *_  = self.repairllama.model.model.rotary_emb(repairllama_h, position_ids=repairllama_position_ids)
-        print("after rotary_emb", repairllama_h)
+        # repairllama_h, *_  = self.repairllama.model.model.rotary_emb(repairllama_h, position_ids=repairllama_position_ids)
+        # print("after rotary_emb", repairllama_h)
         repairllama_output = self.repairllama.model.lm_head(repairllama_h)
 
         # Processing CodeLLama output
