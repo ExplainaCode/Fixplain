55af9841f10e69fc6a5807668f46e63e405dd07e
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-06 13:52:44 +0530

    generate initial

--- adapter.py
+++ adapter.py
@@ -198,6 +198,7 @@ class LLamaAdapter(nn.Module):
             codellama_mask = None
             codellama_mask = torch.full((1, 1, codellama_seqlen, codellama_seqlen), float("-inf"), device=codellama_h.device)
             codellama_mask = torch.triu(codellama_mask, diagonal=0 + 1).type_as(repairllama_h)
+            print("repairllama_mask", repairllama_mask)
 
         assert self.repairllama.config.num_hidden_layers==self.codellama.config['num_hidden_layers']
         n_layers = self.repairllama.config.num_hidden_layers
