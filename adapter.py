0ec19a27051e767136fbf1e3008dfe53fd567ee8
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-06 15:35:09 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -218,8 +218,8 @@ class LLamaAdapter(nn.Module):
 
         # Processing RepairLLama output
         repairllama_h  = self.repairllama.model.model.norm(repairllama_h[0])
-        repairllama_h, *_  = self.repairllama.model.model.rotary_emb(repairllama_h, position_ids=repairllama_position_ids)
-        print("after rotary_emb", repairllama_h)
+        # repairllama_h, *_  = self.repairllama.model.model.rotary_emb(repairllama_h, position_ids=repairllama_position_ids)
+        # print("after rotary_emb", repairllama_h)
         repairllama_output = self.repairllama.model.lm_head(repairllama_h) # remove this for get row logits
 
         if adaptor:
