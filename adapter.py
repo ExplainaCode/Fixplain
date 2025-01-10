1c96abc82046aadf80cabd52fd27bc27f8a839e4
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-10 13:39:11 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -214,7 +214,7 @@ class LLamaAdapter(nn.Module):
         print("repairllama_past_key_values: ", repairllama_past_key_values.__len__())
 
         for i in range(n_layers):
-            past_key_values = repairllama_past_key_values.get(i, None)
+            past_key_values = repairllama_past_key_values.__getitem__(i, None)
             if past_key_values:
                 past_key_values = tuple(pkv.contiguous() for pkv in past_key_values)
             repairllama_h, next_repairllama_cache, *_ = self.repairllama.model.model.layers[i](
