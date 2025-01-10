95cbdbc8329a5fff56347805bb1e930e2667e8dd
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-10 11:25:13 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -282,15 +282,14 @@ class LLamaAdapter(nn.Module):
         codellama_tokens = torch.full((bsz, total_codellama_len), 0).cuda().long() # 0 used instead of self.codellama_tokenizer.pad_id for testing
 
         for k, t in enumerate(repairllama_input_ids):
-            print(t)
-            print(t[0])
-            print(len(t[0]))
             repairllama_tokens[k, : len(t[0])] = torch.tensor(t).cuda().long()
 
         input_repairllama_text_mask = repairllama_tokens != self.repairllama_tokenizer.pad_token_id
         repairllama_start_pos = min_repairllama_prompt_size
 
         for k, t in enumerate(codellama_input_ids):
+            print(t)
+            print(t[0])
             codellama_tokens[k, : len(t[0])] = torch.tensor(t).cuda().long() # cuda
 
         input_codellama_text_mask = codellama_tokens != 0 # o used instead of self.codellama_tokenizer.pad_id for testing
