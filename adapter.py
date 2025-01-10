11ce4806f3438ac132348021bb014c18c25015b9
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-10 11:49:37 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -209,7 +209,7 @@ class LLamaAdapter(nn.Module):
 
         for i in range(n_layers):
             repairllama_h, next_repairllama_cache, *_ = self.repairllama.model.model.layers[i](
-                                                repairllama_h, repairllama_mask, repairllama_position_ids, repairllama_past_key_values
+                                                repairllama_h, repairllama_mask, repairllama_position_ids, repairllama_past_key_values, use_cache=True
                                             )  # Do not pass as keyword arguments since hooks don't capture inputs.        
             assert(self.attention_hooks_data.get(i)!=None)
             # print(self.attention_hooks_data)
