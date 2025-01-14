daea07bab351ebbc7f6c10c2f6267b4b039cfa77
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-15 00:08:13 +0530

    minor changes

--- test.py
+++ test.py
@@ -23,8 +23,8 @@ def main(args):
     seq_len = 128
     vocab_size = 32000  # Adjust as per the tokenizer used
     
-    repairllama_input_ids = torch.load(args.repairllama_input_ids)
-    codellama_input_ids = torch.load(args.codellama_input_ids)
+    repairllama_input_ids = torch.flattern(torch.load(args.repairllama_input_ids))
+    codellama_input_ids = torch.flattern(torch.load(args.codellama_input_ids))
 
     start_pos = 0
 
