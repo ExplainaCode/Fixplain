15bc9f14cfc9523ee845627888cc196c8b73659b
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-15 00:37:20 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -260,6 +260,11 @@ class LLamaAdapter(nn.Module):
             print(repairllama_h)
             print(repairllama_mask)
             print(repairllama_position_ids)
+
+            print(f"repairllama_h.shape: {repairllama_h.shape}, dtype: {repairllama_h.dtype}")
+            print(f"repairllama_mask.shape: {repairllama_mask.shape}, dtype: {repairllama_mask.dtype}")
+            print(f"repairllama_position_ids.shape: {repairllama_position_ids.shape}, dtype: {repairllama_position_ids.dtype}")
+
             repairllama_h, next_repairllama_cache, *_ = self.repairllama.model.model.layers[i](
                                                 repairllama_h.contiguous(), repairllama_mask.contiguous(), repairllama_position_ids.contiguous(), past_key_values, use_cache=True
                                             )  # Do not pass as keyword arguments since hooks don't capture inputs.        
