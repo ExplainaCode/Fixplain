0f9fd35dfa9f32cd2f15cbd19f9980a106afbb2d
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-15 01:45:26 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -234,7 +234,7 @@ class LLamaAdapter(nn.Module):
             codellama_freq_cis = codellama_freq_cis[:codellama_seqlen]
             codellama_mask = None
             codellama_mask = torch.full((1, 1, codellama_seqlen, codellama_seqlen), float("-inf"), device=codellama_h.device)
-            codellama_mask = torch.triu(codellama_maskshape, diagonal=0 + 1).type_as(repairllama_h) #This should change
+            codellama_mask = torch.triu(codellama_mask, diagonal=0 + 1).type_as(repairllama_h) #This should change
 
         assert self.repairllama.config.num_hidden_layers==self.codellama.config['num_hidden_layers']
         n_layers = self.repairllama.config.num_hidden_layers
