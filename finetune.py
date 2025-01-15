c31601eee84d0de08102ff3392c4a6a6f4205b8a
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-15 17:30:31 +0530

    minor changes

--- finetune.py
+++ finetune.py
@@ -53,6 +53,7 @@ def train_one_epoch(model: LLamaAdapter,
             codellama_loss = model.forward(reapirllama_examples, codellama_examples,
                                                               repairllama_labels=repairllama_labels,
                                                               codellama_labels=codellama_labels,)
+        codellama_loss.backword()
         codellama_loss_value = codellama_loss.item()
         if not math.isfinite(codellama_loss_value):
             print("Loss is {}, stopping training".format(codellama_loss_value))
