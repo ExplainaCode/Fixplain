e8d474c598b5c202391be82b412b8ffa36de5274
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-15 19:16:56 +0530

    minor changes

--- finetune.py
+++ finetune.py
@@ -64,7 +64,7 @@ def train_one_epoch(model: LLamaAdapter,
 
         # loss_scaler(loss, optimizer, parameters=model.parameters(),
         #             update_grad=(data_iter_step + 1) % accum_iter == 0)
-        loss.backward()
+        # loss.backward()
         if (data_iter_step + 1) % accum_iter == 0:
             optimizer.zero_grad()
 
