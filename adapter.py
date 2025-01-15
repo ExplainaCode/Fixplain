e073015e45e31177919663a12d5eb103abe931ed
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-15 19:09:46 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -106,10 +106,9 @@ class LLamaAdapter(nn.Module):
         Hook function to capture inputs of attention layers.
         """
         layer_id = module.layer_id
-        print(input[0].requires_grad)
         self.attention_hooks_data[layer_id] = { # {0:{"input": (x, )}}
             # "input": tuple(inp.detach() for inp in input),
-            "input": input[0].detach(),
+            "input": input[0],
         }
 
     def set_trainale_params(self, phase='inference'):
