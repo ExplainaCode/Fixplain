02b2908333b873e032115a6d9fe971d114b416d4
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-15 20:08:35 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -41,7 +41,7 @@ class LLamaAdapter(nn.Module):
         
         self.criterion = torch.nn.CrossEntropyLoss(ignore_index=0)
         self.phase = phase
-        self.set_trainale_params(self.phase)
+        # self.set_trainale_params(self.phase)
 
     def _load_codellama(self, codellama_ckpt_dir, max_seq_len, max_batch_size, codellama_tokenizer, w_lora, lora_rank):
         with open(os.path.join(codellama_ckpt_dir, "params.json"), 'r') as f:
