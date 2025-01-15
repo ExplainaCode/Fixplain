81badb5d1f944224767a3f12cabc896c161b5260
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-15 18:10:58 +0530

    minor changes

--- finetune.py
+++ finetune.py
@@ -62,7 +62,7 @@ def train_one_epoch(model: LLamaAdapter,
         codellama_loss = codellama_loss / accum_iter
 
         loss_scaler(codellama_loss, optimizer, parameters=model.parameters(),
-                    update_grad=(data_iter_step + 1) % accum_iter == 0, retain_graph=True)
+                    update_grad=(data_iter_step + 1) % accum_iter == 0, create_graph=True)
         if (data_iter_step + 1) % accum_iter == 0:
             optimizer.zero_grad()
 
