56ffef823fe8be6b5f6cc4915b9207a4669bb5ed
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-17 08:44:08 +0530

    minor changes

--- finetune.py
+++ finetune.py
@@ -49,7 +49,7 @@ def train_one_epoch(model: LLamaAdapter,
         if data_iter_step % accum_iter == 0:
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
 
-        with torch.cuda.amp.autocast():
+        with torch.amp.autocast("cuda"):
             loss = model(reapirllama_examples, codellama_examples,
                                                             repairllama_labels=repairllama_labels,
                                                             codellama_labels=codellama_labels,)
