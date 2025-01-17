3daceb2be0786a6c574d71aaf85baff264efdcc2
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-17 09:04:48 +0530

    minor changes

--- codellama/model.py
+++ codellama/model.py
@@ -307,7 +307,7 @@ class Attention(nn.Module):
         if adapter is not None:
             if adapter_len > 1:
                 adapter_scores = torch.matmul(xq, adapter_k.transpose(2, 3)) / math.sqrt(self.head_dim)
-                print(adapter_scores)
+                print("_________________________adapter scores:\n", adapter_scores)
                 adapter_scores = self.gate.tanh() * F.softmax(adapter_scores.float(), dim=-1).type_as(xq)
                 # adapter_scores = self.gate.tanh() * F.softmax(adapter_scores.float(), dim=-1).to(torch.float16)
 
