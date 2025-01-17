58a293466dc322a6ec918833197d7bf864e2041d
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-01-17 09:43:12 +0530

    minor changes

--- finetune.py
+++ finetune.py
@@ -70,7 +70,7 @@ def train_one_epoch(model: LLamaAdapter,
         loss.backward()
         if (data_iter_step + 1) % accum_iter == 0:
             print("Loss is: ",loss)
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
             optimizer.zero_grad()      
 
