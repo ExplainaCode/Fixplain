2957f0363813f2cc739befa9122c8a56d8efa583
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-02-03 14:05:59 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -68,6 +68,8 @@ class LLamaAdapter(nn.Module):
         ckpts = sorted(Path(codellama_ckpt_dir).glob("*.pth"))
         for ckpt_path in ckpts:
             ckpt = torch.load(ckpt_path, map_location="cpu")
+            if "model" in ckpt: #for the later loading
+                ckpt = ckpt["model"]
             missing_keys, unexpected_keys = codellama.load_state_dict(ckpt, strict=False)
 
             print(f"Checkpoint: {ckpt_path}")
