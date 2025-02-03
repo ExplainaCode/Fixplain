5f49cacfa6439ce816b88cedf70f03f10ba94a22
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-02-03 15:02:20 +0530

    minor changes

--- utils/misc.py
+++ utils/misc.py
@@ -312,9 +312,11 @@ def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
     print("_______________trainable_state_dict_______________")
     print(trainable_state_dict.keys())
 
-    # print("Inside Save Keys in trainable_state_dict:")
-    # for key in trainable_state_dict.keys():
-    #     print(key)
+    print("Inside Save Keys in trainable_state_dict:")
+    for name, param in model_without_ddp.named_parameters():
+        if param.requires_grad:
+            print(name, param.grad)
+            break
         
     # Prepare the checkpoint dictionary
     checkpoint = {
