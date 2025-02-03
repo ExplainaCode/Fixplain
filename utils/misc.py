b9a2f1a376f35b1dcda3f3a3a10c0776c690e2fc
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-02-03 14:39:00 +0530

    minor changes

--- utils/misc.py
+++ utils/misc.py
@@ -306,9 +306,10 @@ def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
     # Extract only updated (trainable and changed) parameters
     trainable_state_dict = {name: param for name, param in model_without_ddp.named_parameters() 
                             if param.requires_grad and param.grad is not None}
-    for name, param in model_without_ddp.named_parameters():
-        print("_______________in model saving______________________-")
-        print(name)
+    # for name, param in model_without_ddp.named_parameters():
+    #     print("_______________in model saving______________________-")
+    #     print(name)
+    print(trainable_state_dict.keys())
 
     print("Inside Save Keys in trainable_state_dict:")
     for key in trainable_state_dict.keys():
