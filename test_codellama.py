50ebfc81f47ffde260167b4af6b94adb3afb7b33
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-02-05 15:19:25 +0530

    minor changes

--- test_codellama.py
+++ test_codellama.py
@@ -32,7 +32,7 @@ def load_codellama_ddp(rank, world_size, codellama_ckpt_dir, max_seq_len, max_ba
     model_args.vocab_size = tokenizer.n_words
     
     torch.set_default_tensor_type(torch.cuda.HalfTensor)
-    codellama = Transformer(model_args).to(rank).half()
+    codellama = Transformer(model_args).to("cpu").half()
 
     # Load checkpoint
     ckpts = sorted(Path(codellama_ckpt_dir).glob("*.pth"))
