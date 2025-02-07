94e0b1518e4112e714e34713e4914b895a9674d3
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-02-07 10:42:29 +0530

    minor changes

--- test_codellama.py
+++ test_codellama.py
@@ -182,7 +182,10 @@ def generate(codellama, codellama_tokenizer, codellama_input_ids=None,
 
     if isinstance(codellama_input_ids[0], str):
         # This has custom tokenizer encode in codellama directory
-        codellama_input_ids = [torch.tensor(codellama_tokenizer.encode(x, bos=True, eos=False) for x in codellama_input_ids)]
+        codellama_input_ids = [
+            torch.tensor(codellama_tokenizer.encode(x, bos=True, eos=False)).unsqueeze(0)  # Shape: (1, sequence_length)
+            for x in codellama_input_ids
+        ]
     print(codellama_input_ids)
     #Clipplig to max_seq_len
     # Convert list of tensors into a single tensor
