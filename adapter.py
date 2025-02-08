3036682e89efd9b50d82982efe10d7792c09cfce
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-02-08 10:29:50 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -354,8 +354,8 @@ class LLamaAdapter(nn.Module):
         repairllama_input_ids = repairllama_input_ids[:, :, :params.max_seq_len]
         codellama_input_ids = codellama_input_ids[:, :, :params.max_seq_len]
 
-        print("repairllama_input_ids: ", repairllama_input_ids)
-        print("codellama_input_ids: ", codellama_input_ids)
+        # print("repairllama_input_ids: ", repairllama_input_ids)
+        # print("codellama_input_ids: ", codellama_input_ids)
 
         min_repairllama_prompt_size = min([len(t[0]) for t in repairllama_input_ids])
         max_repairllama_prompt_size = max([len(t[0]) for t in repairllama_input_ids])
