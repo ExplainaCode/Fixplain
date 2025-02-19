178d540799d706b267c464b04fed84b72882ee76
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-02-19 06:13:50 +0530

    new branch for only lora training

--- codellama/model.py
+++ codellama/model.py
@@ -245,7 +245,8 @@ class Attention(nn.Module):
         if adapter is not None:
             if adapter_len > 1:
                 adapter_scores = torch.matmul(xq, adapter_k.transpose(2, 3)) / math.sqrt(self.head_dim)
-                adapter_scores = self.gate.tanh() * F.softmax(adapter_scores.float(), dim=-1).type_as(xq)
+                adapter_scores = F.softmax(adapter_scores.float(), dim=-1).type_as(xq)
+                adapter_scores = self.gate.tanh() * adapter_scores
 
                 if self.w_new_gate:
                     adapter_scores = self.new_gate * adapter_scores
