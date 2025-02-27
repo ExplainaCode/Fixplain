5aab8eddb1e406bb237353a5de8e707c795b9364
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-02-27 11:37:08 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -234,6 +234,8 @@ class LLamaAdapter(nn.Module):
             with torch.no_grad():
                 dynamic_adapter = self.attention_hooks_data[i].get('input')#.half() # Hooked input to the respective repairllama layer
                 dynamic_adapter = dynamic_adapter.to(dtype=codellama_h.dtype)
+                if torch.isnan(dynamic_adapter).any() or torch.isinf(dynamic_adapter).any():
+                    raise ValueError("dynamic adapter contains NaN or inf values.___________0", i)
             # del self.attention_hooks_data[i]
             self.attention_hooks_data[i] = None
             codellama_h = self.codellama.layers[i](codellama_h, 0, codellama_freq_cis, codellama_mask, dynamic_adapter)
