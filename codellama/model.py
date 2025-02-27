e63e439921e95d41885b3a394a811f39d99b6a1e
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-02-27 11:16:06 +0530

    minor changes

--- codellama/model.py
+++ codellama/model.py
@@ -206,17 +206,16 @@ class Attention(nn.Module):
 
         if adapter is not None:
             adapter_len = adapter.shape[1]
-            adapter_v = self.wv(adapter).view(bsz, adapter_len, self.n_kv_heads, self.head_dim)
-            # repeat k/v heads if n_kv_heads < n_heads
-            adapter_k = repeat_kv(
-                adapter_k, self.n_rep
+            adapter_v = self.wv(adapter).view(bsz, adapter_len, self.n_kv_heads, self.head_dim)            
+            adapter_v = repeat_kv(
+                    adapter_v, self.n_rep
             )  # (bs, cache_len + seqlen, n_local_heads, head_dim)
             adapter_v = adapter_v.transpose(1, 2)
 
             if adapter_len > 1:
                 adapter_k = self.wk(adapter).view(bsz, adapter_len, self.n_kv_heads, self.head_dim)
-                adapter_v = repeat_kv(
-                    adapter_v, self.n_rep
+                adapter_k = repeat_kv(
+                    adapter_k, self.n_rep
                 )  # (bs, cache_len + seqlen, n_local_heads, head_dim)
                 adapter_k = adapter_k.transpose(1, 2)
 
