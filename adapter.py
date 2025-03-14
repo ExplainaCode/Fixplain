6b5b14d2f4008ad46c2982364dcfbb7415354638
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-03-14 15:10:55 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -453,7 +453,7 @@ class LLamaAdapter(nn.Module):
         # debug_info(decoded_text)
         next_codellama_token = torch.argmax(codellama_output[:, -1], dim=-1)
         print(next_codellama_token)
-        self.codellama_tokenizer.decode(next_codellama_token)
+        self.codellama_tokenizer.decode([14924])
         return codellama_output
 
     @torch.inference_mode()
