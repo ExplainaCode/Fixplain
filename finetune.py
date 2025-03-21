04ab93ba2d92ba6207831d6f8cb1e26a36b0c2c5
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-03-21 10:27:16 +0530

    minor changes

--- finetune.py
+++ finetune.py
@@ -97,12 +97,12 @@ def train_one_epoch(model: LLamaAdapter,
         #             update_grad=(data_iter_step + 1) % accum_iter == 0)
 
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         if (data_iter_step + 1) % accum_iter == 0:
             # for name, param in model.named_parameters():
             #     if param.grad is not None and torch.isnan(param.grad).any():
             #         print(f"NaN detected in gradients for {name}!")
             #         raise ValueError("NaN in gradients.")
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
             optimizer.zero_grad() 
             # flag=False
