7aa6e15ed408a766b08accb6da0ca42e2b9329bd
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-03-23 11:18:32 +0530

    minor changes

--- finetune.py
+++ finetune.py
@@ -93,13 +93,13 @@ def train_one_epoch(model: LLamaAdapter,
         #     print("Loss contains NaNs or Infs:", loss)
         #     sys.exit(1)
 
-        loss_scaler(loss, optimizer, clip_grad=1.0, parameters=model.parameters(),
-                    update_grad=(data_iter_step + 1) % accum_iter == 0)
+        # loss_scaler(loss, optimizer, clip_grad=1.0, parameters=model.parameters(),
+        #             update_grad=(data_iter_step + 1) % accum_iter == 0)
 
-        # loss.backward()
+        loss.backward()
         if (data_iter_step + 1) % accum_iter == 0:
-            # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-            # optimizer.step()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            optimizer.step()
             optimizer.zero_grad() 
 
             # flag=False
