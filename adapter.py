bcf7d5cad61bdc404568f6860fc00a874367f9fb
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-03-23 08:52:15 +0530

    minor changes

--- adapter.py
+++ adapter.py
@@ -152,6 +152,9 @@ class LLamaAdapter(nn.Module):
         ckpt = torch.load(ckpt_path, map_location="cpu") # This ckeckpoint contains other parameters as well
         ckpt = ckpt["model"] 
         missing_keys, unexpected_keys = self.codellama.load_state_dict(ckpt, strict=False)
+        
+        for name, param in self.codellama.state_dict().items():
+            print(f"Parameter: {name}, dtype: {param.dtype}")
 
         # debug_info("____________________in trained weights loading___________________")
         # print(f"Checkpoint: {ckpt_path}")
