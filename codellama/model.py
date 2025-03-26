077962605b153fd9e47ee45e113e09f54e8ecd0c
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-03-26 21:01:03 +0530

    minor changes

--- codellama/model.py
+++ codellama/model.py
@@ -120,6 +120,7 @@ class Attention(nn.Module):
         self.n_rep = self.n_local_heads // self.n_local_kv_heads
         self.head_dim = args.dim // args.n_heads
         self.w_lora = args.w_lora
+        self.args=args
 
         self.wq = ColumnParallelLinear(
             args.dim,
@@ -199,6 +200,19 @@ class Attention(nn.Module):
             # self.lora_wv_l2.weight.data = self.lora_wv_l2.weight.data.to(torch.float16)
             # self.lora_wo_l2.weight.data = self.lora_wo_l2.weight.data.to(torch.float16)
         torch.set_default_tensor_type(original_tensor_type)
+
+        def train(self, mode: bool = True):
+            if mode:
+                self.cache_k = None
+                self.cache_v = None
+            else:
+                self.cache_k = torch.zeros(
+                    (self.args.max_batch_size, self.args.max_seq_len, self.n_local_heads, self.head_dim)
+                ).cuda()
+                self.cache_v = torch.zeros(
+                    (self.args.max_batch_size, self.args.max_seq_len, self.n_local_heads, self.head_dim)
+                ).cuda()
+            return super().train(mode)
 
     def forward(
         self,
