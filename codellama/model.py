7dad1c1445dfded27d7c59b57f24fb4afbbabc7d
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-03-26 20:11:19 +0530

    minor changes

--- codellama/model.py
+++ codellama/model.py
@@ -268,8 +268,8 @@ class Attention(nn.Module):
         if adapter is not None:
             if adapter_len > 1:
                 logits = torch.matmul(xq, adapter_k.transpose(2, 3)) / math.sqrt(self.head_dim)
-                logits = torch.clamp(logits, min=-100, max=100)
-                logits = logits - logits.max(dim=-1, keepdim=True)[0]
+                # logits = torch.clamp(logits, min=-100, max=100)
+                # logits = logits - logits.max(dim=-1, keepdim=True)[0]
                 # print(f"Logits - min: {logits.min().item()}, max: {logits.max().item()}, mean: {logits.mean().item()}")
                 # print(adapter_scores.type(), "adapter scores 1")
                 adapter_scores = F.softmax(logits, dim=-1) #.type_as(xq)
