ef58e7ca6aeb8479ba40393d68d501f77727576e
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-03-26 11:06:26 +0530

    minor changes

--- codellama/model.py
+++ codellama/model.py
@@ -275,8 +275,8 @@ class Attention(nn.Module):
                 # print_tensor_stats(gate_tanh, "tanh(gate)")
                 # print_tensor_stats(adapter_scores, "adapter_scores (before multiplication)")
 
-                # adapter_scores = self.gate.tanh().float() * adapter_scores.float()
-                adapter_scores.mul_(self.gate.tanh())
+                adapter_scores = self.gate.tanh().float() * adapter_scores.float()
+                # adapter_scores.mul_(self.gate.tanh())
                 # adapter_scores.clamp_(min=1e-7)
                 # check_tensor_abnormalities(adapter_scores, "final_adapter_scores")
 
