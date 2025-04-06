45eced0db6ad80de1a37fd38423b6165ad221eed
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-04-07 05:10:44 +0530

    minor changes

--- utils/dataset.py
+++ utils/dataset.py
@@ -71,30 +71,11 @@ class FinetuneDataset(Dataset):
                 return_tensors='pt'
             )
             repairllama_input_ids = repairllama_encoding['input_ids'].squeeze(0)  # [max_len]
-
-            # repairllama_label_encoding = self.repairllama_tokenizer.encode_plus(
-            #     fixed_code,
-            #     max_length=self.repairllama_max_output_len,
-            #     padding='max_length',
-            #     truncation=True,
-            #     return_tensors='pt'
-            # )
-            # repairllama_label_ids = repairllama_label_encoding['input_ids'].squeeze(0)
-            # repairllama_input_ids =  torch.flatten(self.repairllama_tokenizer.encode(buggy_code, return_tensors='pt'))
-            # repairllama_label_ids = torch.flatten(self.repairllama_tokenizer.encode(fixed_code, return_tensors='pt'))
             codellama_input_ids = torch.tensor(self.codellama_tokenizer.encode(explanation, bos=True, eos=False))
-
-            # repairllama_input_ids = self.__get_padding__(repairllama_input_ids, self.repairllama_pad_id, self.repairllama_max_input_len)
-            # repairllama_label_ids = self.__get_padding__(repairllama_label_ids, self.repairllama_pad_id, self.repairllama_max_output_len)
             codellama_input_ids = self.__get_padding__(codellama_input_ids, self.codellama_pad_id, self.codellama_max_input_len)
 
             codellama_label_ids = copy.deepcopy(codellama_input_ids)
             codellama_input_ids_mask  = codellama_input_ids.ge(0) # just for keep functions work for now - no need !
-            # codellama_label_mask = codellama_label_ids.ge(0)
-            # codellama_input_ids[~codellama_input_ids_mask] = 0
-            # codellama_label_ids[~codellama_label_mask] = 0
-            # codellama_label_mask = codellama_label_mask.float()
-            # codellama_input_ids_mask = codellama_input_ids_mask.float()
 
             return repairllama_input_ids, codellama_input_ids, codellama_label_ids, codellama_input_ids_mask
         
