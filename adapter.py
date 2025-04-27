b734fc987967eb34d47c7ea470782842244dcce4
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-04-27 12:54:33 +0530

    cleaned adapter

--- adapter.py
+++ adapter.py
@@ -340,79 +340,7 @@ class LLamaAdapter(nn.Module):
         return llama_c_loss
     
     @torch.inference_mode()
-    def forward_inference(self, repairllama_input_ids, llama_input_ids, 
-                          repairllama_start_pos: int,llama_start_pos:int, 
-                          repairllama_past_key_values=None, adapter=False):
-
-        repairllama_input_ids=repairllama_input_ids.to(device) #Decide whether this is the optimal position to move to the device #probably in training we can directly load to the device at once?
-        if adapter:
-            llama_input_ids=llama_input_ids.to(device)
-        _bsz, repairllama_seqlen = repairllama_input_ids.shape
-
-        repairllama_h = self.repairllama.model.model.embed_tokens(repairllama_input_ids) # apass through embedding layer
-
-        # repairllama_freqs_cis = self.repairllama.freqs_cis.to(repairllama_h.device) 
-        # repairllama_freqs_cis = repairllama_freqs_cis[:repairllama_seqlen]
-        repairllama_position_ids = torch.arange(repairllama_seqlen, dtype=torch.long, device=repairllama_input_ids.device).unsqueeze(0).expand(_bsz, -1)
-        repairllama_mask = None
-        repairllama_mask = torch.full((1, 1, repairllama_seqlen, repairllama_seqlen), float("-inf"), device=repairllama_h.device)
-        repairllama_mask = torch.triu(repairllama_mask, diagonal=repairllama_start_pos + 1).type_as(repairllama_h) #might this cause issues?.
-
-        if adapter:
-            # LLama configuration before forward pass # This is redundent if works movw to a function or something...
-            _bsz, llama_seqlen = llama_input_ids.shape
-            llama_h = self.llama.tok_embeddings(llama_input_ids)
-            llama_freq_cis = self.llama.freqs_cis.to(llama_h.device)
-            llama_freq_cis = llama_freq_cis[:llama_seqlen]
-            llama_mask = None
-            llama_mask = torch.full((1, 1, llama_seqlen, llama_seqlen), float("-inf"), device=llama_h.device)
-            llama_mask = torch.triu(llama_mask, diagonal=llama_start_pos + 1).type_as(repairllama_h)
-
-        assert self.repairllama.config.num_hidden_layers==self.llama.config['num_hidden_layers']
-        n_layers = self.repairllama.config.num_hidden_layers
-
-        if repairllama_past_key_values is None:
-            from transformers.cache_utils import DynamicCache
-            repairllama_past_key_values = DynamicCache()
-
-        repairllama_past_key_values_len = repairllama_past_key_values.__len__()
-        for i in range(n_layers):
-            if i < repairllama_past_key_values_len:
-                past_key_values = repairllama_past_key_values.__getitem__(i)
-            else:
-                past_key_values = None
-            if past_key_values:
-                past_key_values = tuple(pkv.contiguous() for pkv in past_key_values)
-
-            repairllama_h, next_repairllama_cache, *_ = self.repairllama.model.model.layers[i](
-                                                repairllama_h.contiguous(), repairllama_mask.contiguous(), repairllama_position_ids.contiguous(), past_key_values, use_cache=True
-                                            )  # Do not pass as keyword arguments since hooks don't capture inputs.        
-            assert(self.attention_hooks_data.get(i)!=None)
-
-            if adapter:
-                dynamic_adapter  = self.attention_hooks_data[i].get('input') # Hooked input to the respective repairllama layer
-                llama_h = self.llama.layers[i](llama_h, llama_start_pos, llama_freq_cis, llama_mask, dynamic_adapter)
-
-        self.attention_hooks_data={} # Resetting can also be done in the above loop. 
-
-
-        # Processing RepairLLama output
-        repairllama_h  = self.repairllama.model.model.norm(repairllama_h)
-        # repairllama_h, *_  = self.repairllama.model.model.rotary_emb(repairllama_h, position_ids=repairllama_position_ids)
-        # print("repairllama shape 3: ", repairllama_h[:, -1, :].shape)
-        repairllama_output = self.repairllama.model.lm_head(repairllama_h[:, -1, :])  # We assume that lm_lead accepts (batch_size, voc_size), not (batch_size, seq_len, voc_size) check this.
-
-        if adapter:
-            # Processing LLama output
-            llama_h = self.llama.norm(llama_h)
-            llama_output = self.llama.output(llama_h[:,-1, :])
-        else: 
-            llama_output = None
-
-        return repairllama_output, llama_output.float() if llama_output is not None else None, next_repairllama_cache
-
-    @torch.inference_mode()
-    def forward_inference_2(self, llama_input_ids,llama_start_pos:int):
+    def forward_inference(self, llama_input_ids,llama_start_pos:int):
         llama_input_ids=llama_input_ids.to(device)
 
         _bsz, llama_seqlen = llama_input_ids.shape
@@ -491,7 +419,7 @@ class LLamaAdapter(nn.Module):
                                                 repairllama_h.contiguous(), repairllama_mask.contiguous(), repairllama_position_ids.contiguous()
                                             )  # Do not pass as keyword arguments since hooks don't capture inputs.
     @torch.inference_mode()
-    def generate_2(self, repairllama_input_ids, llama_input_ids=None,
+    def generate(self, repairllama_input_ids, llama_input_ids=None,
                    max_gen_len: int=256, max_llama_gen_len: int=125, temperature: float=0.1,
                    top_p:  float=0.75):
         bsz = len(repairllama_input_ids)
@@ -545,7 +473,7 @@ class LLamaAdapter(nn.Module):
         # i = 0
         for cur_pos in range(llama_start_pos, total_llama_len):  
             with torch.cuda.amp.autocast():
-                llama_logits = self.forward_inference_2(llama_tokens[:, prev_pos:cur_pos], prev_pos)
+                llama_logits = self.forward_inference(llama_tokens[:, prev_pos:cur_pos], prev_pos)
             if temperature > 0:
                 probs = torch.softmax(llama_logits[:, -1] / temperature, dim=-1)
                 next_llama_token = sample_top_p(probs, top_p)
@@ -581,156 +509,3 @@ class LLamaAdapter(nn.Module):
             llama_decoded.append(self.llama_tokenizer.decode(t))
 
         return repairllama_input_ids, llama_decoded
-
-        
-    @torch.inference_mode()
-    def generate(self, repairllama_input_ids, llama_input_ids=None,
-                 max_gen_len: int=256, max_llama_gen_len:int=125, temperature: float=0.1,
-                 top_p: float=0.75):
-        bsz = len(repairllama_input_ids)
-        if llama_input_ids==None:
-            llama_input_ids = [
-                torch.full((1, 1), fill_value=self.llama_tokenizer.pad_id, dtype=torch.long) #  torch.full((1, seq_len), fill_value=0, dtype=torch.long) 
-                for _ in range(bsz)
-            ]
-        assert len(repairllama_input_ids)==len(llama_input_ids) #batch sizes should be equal.
-       
-       # is this need to be checked. because batch sizes of both inputs are equal and both use same model. hece comment down and 
-       # create a single params. check this
-        # repairllama_params = self.repairllama.params
-        # llama_params = self.llama.params
-        # assert bsz <= repairllama_params.max_batch_size, (bsz, repairllama_params.max_batch_size)
-        # assert bsz <= llama_params.max_batch_size, (bsz, llama_params.max_batch_size)
-
-        # Replaced with params,
-        params = self.llama.params
-        assert bsz <= params.max_batch_size, (bsz, params.max_batch_size)
-
-        if isinstance(repairllama_input_ids[0], str): # if the inputs are given as strings instead of input_ids
-             #This assumes list of pytorch tensors returns given enumerable (list) of input texts.
-            repairllama_input_ids = [self.repairllama_tokenizer.encode(x, return_tensors='pt') for x in repairllama_input_ids]
-        
-        if isinstance(llama_input_ids[0], str):
-            # This has custom tokenizer encode in llama directory
-            llama_input_ids = [self.llama_tokenizer.encode(x, bos=True, eos=False) for x in llama_input_ids]
-        
-        #Clipplig to max_seq_len
-        # Convert list of tensors into a single tensor
-        repairllama_input_ids = torch.stack(repairllama_input_ids)
-        llama_input_ids = torch.stack(llama_input_ids)
-        repairllama_input_ids = repairllama_input_ids[:, :, :params.max_seq_len]
-        llama_input_ids = llama_input_ids[:, :, :params.max_seq_len]
-
-        # print("repairllama_input_ids: ", repairllama_input_ids)
-        # print("llama_input_ids: ", llama_input_ids)
-
-        min_repairllama_prompt_size = min([len(t[0]) for t in repairllama_input_ids])
-        max_repairllama_prompt_size = max([len(t[0]) for t in repairllama_input_ids])
-        min_llama_prompt_size = min([len(t[0]) for t in llama_input_ids])
-        max_llama_prompt_size = max([len(t[0]) for t in llama_input_ids])
-
-        # max_llama_gen_len = max_gen_len # max_llama_gen_len should be taken from the parameters, for the testing it is equal to the max_gen_len (in repairllama)
-        total_repairllama_len = min(params.max_seq_len, max_gen_len + max_repairllama_prompt_size)
-        repairllama_tokens = torch.full((bsz, total_repairllama_len), self.repairllama_tokenizer.pad_token_id).cuda().long()
-        total_llama_len = min(params.max_seq_len, max_llama_gen_len + max_llama_prompt_size) # instead of generic params.max_seq_len consider using specific to llama & max_gen_len for llama text.
-        llama_tokens = torch.full((bsz, total_llama_len), self.llama_tokenizer.pad_id).cuda().long() # 0 used instead of self.llama_tokenizer.pad_id for testing
-
-        for k, t in enumerate(repairllama_input_ids):
-            if total_repairllama_len <= len(t[0]): #Deugging
-                repairllama_tokens[k, : total_repairllama_len] = torch.tensor(t).cuda().long()
-            else:
-                repairllama_tokens[k, : len(t[0])] = torch.tensor(t).cuda().long()
-
-        input_repairllama_text_mask = repairllama_tokens != self.repairllama_tokenizer.pad_token_id
-        repairllama_start_pos = min_repairllama_prompt_size
-
-        for k, t in enumerate(llama_input_ids):
-            if total_llama_len <=len(t[0]):
-                llama_tokens[k, : total_llama_len] = torch.tensor(t).cuda().long() # cuda
-            else:
-                llama_tokens[k, : len(t[0])] = torch.tensor(t).cuda().long() # cuda
-
-        input_llama_text_mask = llama_tokens != self.llama_tokenizer.pad_id # o used instead of self.llama_tokenizer.pad_id for testing
-        llama_start_pos = min_llama_prompt_size
-        # assert total_repairllama_len >= total_llama_len
-        llama_iter_start_pos = (total_repairllama_len - min_repairllama_prompt_size) - (total_llama_len - min_llama_prompt_size)
-        if llama_iter_start_pos < 0: 
-            llama_iter_start_pos = 0
-
-        prev_pos = 0
-        llama_pre_pos = 0
-        llama_cur_pos=llama_start_pos
-        next_repairllama_cache = None
-        for cur_pos in range(repairllama_start_pos, total_repairllama_len):
-            with torch.cuda.amp.autocast():
-                if cur_pos -repairllama_start_pos  <= llama_iter_start_pos:
-                    repairllama_output, _ , next_repairllama_cache = self.forward_inference(repairllama_tokens[:, prev_pos:cur_pos], None, prev_pos, llama_pre_pos,repairllama_past_key_values=next_repairllama_cache, adapter=False)
-                else:
-                    repairllama_output, llama_logits, next_repairllama_cache = self.forward_inference(repairllama_tokens[:, prev_pos:cur_pos], llama_tokens[:, llama_pre_pos:llama_cur_pos], prev_pos, llama_pre_pos, repairllama_past_key_values=next_repairllama_cache, adapter=True)
-            # print("Repairllama logits: ", repairllama_logits, repairllama_logits.shape)
-            # if temperature > 0:
-            #     probs = torch.softmax(repairllama_logits / temperature, dim=-1)
-            #     next_repairllama_token = sample_top_p(probs, top_p)
-            # else:
-            #     next_repairllama_token = torch.argmax(repairllama_logits, dim=-1)
-            # print("Next_repairllama_token_before modification: ", next_repairllama_token)
-            # print(next_repairllama_token.shape)
-            # next_repairllama_token = repairllama_output.reshape(-1)
-            next_repairllama_token = torch.argmax(repairllama_output, dim=-1) # samplelling is not used naive approach, check this with repairllama huggingface implementation.
-            # print("repairllama_output: ", repairllama_output, repairllama_output.shape)
-            # print("next repairllama token1: ", next_repairllama_token)
-
-            next_repairllama_token = torch.where(
-                input_repairllama_text_mask[:, cur_pos], repairllama_tokens[:, cur_pos], next_repairllama_token
-            )
-
-            repairllama_tokens[:, cur_pos] = next_repairllama_token
-
-            # llama_cur_pos = cur_pos-repairllama_start_pos-llama_iter_start_pos
-
-            if cur_pos - repairllama_start_pos > llama_iter_start_pos:
-                # Then the llama logits are available.
-                if temperature > 0:
-                    probs = torch.softmax(llama_logits / temperature, dim=-1)
-                    next_llama_token = sample_top_p(probs, top_p)
-                else:
-                    next_llama_token = torch.argmax(llama_logits, dim=-1)
-                next_llama_token = next_llama_token.reshape(-1)
-                # print("llama_cur_pos: ", llama_cur_pos)
-                # print("llama_tokens shape: ", llama_tokens.shape)
-                next_llama_token = torch.where(
-                    input_llama_text_mask[:, llama_cur_pos], llama_tokens[:, llama_cur_pos], next_llama_token
-                )
-                llama_tokens[:, llama_cur_pos] = next_llama_token
-                llama_pre_pos=llama_cur_pos
-                llama_cur_pos+=1
-
-            prev_pos = cur_pos
-
-
-        repairllama_decoded = []
-        for i, t in enumerate(repairllama_tokens.tolist()):
-
-            # cut to max gen len
-            t = t[len(repairllama_input_ids[i]): len(repairllama_input_ids[i]) + max_gen_len]
-            # cut to eos tok if any
-            try:
-                t = t[: t.index(self.repairllama_tokenizer.eos_token_id)]
-            except ValueError:
-                pass
-            repairllama_decoded.append(self.repairllama_tokenizer.decode(t))
-        
-        print("llama_tokens: ",  llama_tokens)
-        llama_decoded = []
-        for i, t in enumerate(llama_tokens.tolist()):
-
-            # cut to max gen len
-            t = t[len(llama_input_ids[i]): len(llama_input_ids[i]) + max_gen_len]
-            # cut to eos tok if any
-            try:
-                t = t[: t.index(self.llama_tokenizer.eos_id)]
-            except ValueError:
-                pass
-            llama_decoded.append(self.llama_tokenizer.decode(t))
-
-        return repairllama_decoded, llama_decoded
