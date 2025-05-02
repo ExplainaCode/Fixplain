c06582aa5350ab15311b7c39caaa82a41e391d40
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-05-02 14:00:56 +0530

    minor changes

--- tests_generate_tuned.py
+++ tests_generate_tuned.py
@@ -56,7 +56,7 @@ def main(args):
         
         all_llama_outputs = []
         # Process each input ID in repairllama_input_ids
-        limit=2
+        limit=100
         expected_llama_output= expected_llama_output[:limit]
         for i, repair_input in enumerate(repairllama_input[:limit]):
             print(f"Processing record {i + 1}/{len(repairllama_input[:limit])}...")
