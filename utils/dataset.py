248f46472e87fcd76a0d1ea6883a6602ab9bea5e
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-05-15 18:37:55 +0530

    minor changes

--- utils/dataset.py
+++ utils/dataset.py
@@ -61,6 +61,8 @@ class FinetuneDataset(Dataset):
             fixed_code = row['fixed_code']
             explanation = row['gpt_explanation']
 
+            print(f"Type of buggy_code: {type(buggy_code)}, Value: {buggy_code}")
+
             prompt = PROMPT_DICT["prompt_input"].format_map({"buggy_code": buggy_code})
             example= prompt + explanation
 
