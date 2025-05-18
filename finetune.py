c58a243e1c1b5410c285987e93669b975c3b3b14
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2025-05-18 05:49:29 +0530

    minor changes

--- finetune.py
+++ finetune.py
@@ -35,6 +35,7 @@ def train_one_epoch(model: LLamaAdapter,
                     log_writer=None,
                     args=None):
     model.train(True)
+    model.to("cuda")
     ## model.module.set_default_trainability()
 
     metric_logger = misc.MetricLogger(delimiter="  ")
